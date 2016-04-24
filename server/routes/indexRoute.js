34f894526e35ec57c7c0bf84fc087ddb65355072
TravisIngram <ingramtravis@gmail.com>
2016-04-24 16:39:12 -0500

    Add Path to project, send location of index.html

--- server/routes/indexRoute.js
+++ server/routes/indexRoute.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var path = require('path');
 
 var router = express.Router();
 
 router.get('/', function(request, response) {
-	response.send('Root route established.');
+	response.sendFile(path.join(__dirname, '../public/views/index.html'));
 });
 
 module.exports = router;
