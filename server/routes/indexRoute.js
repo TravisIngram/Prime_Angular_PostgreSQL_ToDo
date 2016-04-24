52882dc5c6197f708d10fa1db9a194ef6306bf9d
TravisIngram <ingramtravis@gmail.com>
2016-04-24 17:53:56 -0500

    Add categoryRoute

--- server/routes/indexRoute.js
+++ server/routes/indexRoute.js
@@ -2,9 +2,12 @@ var express = require('express');
 var path = require('path');
 
 var router = express.Router();
+var categoryRoute = require('./categoryRoute.js');
 
 router.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname, '../public/views/index.html'));
 });
+
+router.use('/categoryRoute', categoryRoute);
 
 module.exports = router;
