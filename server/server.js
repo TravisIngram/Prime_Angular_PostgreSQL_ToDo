17fa3a5cf96b1afc72b865fb65854a49293ad41b
TravisIngram <ingramtravis@gmail.com>
2016-04-24 16:23:11 -0500

    Add indexRoute, bodyParser, set static file location in server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+var indexRoute = require('./routes/indexRoute.js');
+
+app.use(express.static('server/public'));
+app.use(bodyParser.json());
+app.use('/', indexRoute);
 
 var port = process.env.PORT || 3000;
 
