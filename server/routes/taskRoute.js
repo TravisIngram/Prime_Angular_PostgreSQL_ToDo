4be82a000c8eca5b4637ee21bea9b0e20241938f
TravisIngram <ingramtravis@gmail.com>
2016-04-25 11:45:26 -0500

    Add get route, query to retrieve tasks

--- server/routes/taskRoute.js
+++ server/routes/taskRoute.js
@@ -36,3 +36,33 @@ router.post('/', function(request, response) {
 });
 
 module.exports = router;
+
+router.get('/', function(request, response) {
+	console.log(request.body);
+	pg.connect(connectionString, function(err, client, done) {
+		if(err) {
+			console.log(err);
+			response.sendStatus(500);
+		} else {
+			var query = client.query('SELECT "tasks", "status" FROM "task"');
+			var results = [];
+
+			query.on('error', function(error) {
+				console.log(error);
+				response.sendStatus(500);
+				done();
+			});
+
+			query.on('row', function(rowData) {
+				results.push(rowData);
+			});
+
+			query.on('end', function() {
+				response.send(results);
+				done();
+			});
+		}
+	});
+});
+
+module.exports = router;
