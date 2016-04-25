3f8d50d04f4fe79726f88d9edcdbd197ace6de0d
TravisIngram <ingramtravis@gmail.com>
2016-04-25 11:20:25 -0500

    Update file to reflect single table, add columns to task

--- server/models/dbConnection.js
+++ server/models/dbConnection.js
@@ -2,47 +2,47 @@ var pg = require('pg');
 
 var connectionString = 'postgres://localhost:5432/angularSQLToDo';
 
-function initializeStatus() {
-	pg.connect(connectionString, function(err, client, done) {
-		if(err) {
-			console.log(err);
-			process.exit(1);
-		} else {
-			var query = client.query('CREATE TABLE IF NOT EXISTS status ("id" serial PRIMARY KEY, "status" varchar(25) NOT NULL)');
+// function initializeStatus() {
+// 	pg.connect(connectionString, function(err, client, done) {
+// 		if(err) {
+// 			console.log(err);
+// 			process.exit(1);
+// 		} else {
+// 			var query = client.query('CREATE TABLE IF NOT EXISTS status ("id" serial PRIMARY KEY, "status" varchar(25) NOT NULL)');
 
-			query.on('end', function() {
-				console.log('Successfully created schema.');
-				done();
-			});
+// 			query.on('end', function() {
+// 				console.log('Successfully created schema.');
+// 				done();
+// 			});
 
-			query.on('error', function(error) {
-				console.log('Error creating schema.', error);
-				process.exit(1);
-			});
-		}
-	});
-}
+// 			query.on('error', function(error) {
+// 				console.log('Error creating schema.', error);
+// 				process.exit(1);
+// 			});
+// 		}
+// 	});
+// }
 
-function initializeCategory() {
-	pg.connect(connectionString, function(err, client, done) {
-		if(err) {
-			console.log(err);
-			process.exit(1);
-		} else {
-			var query = client.query('CREATE TABLE IF NOT EXISTS category ("id" serial PRIMARY KEY, "catName" varchar(50) NOT NULL)');
+// function initializeCategory() {
+// 	pg.connect(connectionString, function(err, client, done) {
+// 		if(err) {
+// 			console.log(err);
+// 			process.exit(1);
+// 		} else {
+// 			var query = client.query('CREATE TABLE IF NOT EXISTS category ("id" serial PRIMARY KEY, "catName" varchar(50) NOT NULL)');
 
-			query.on('end', function() {
-				console.log('Successfully created schema.');
-				done();
-			});
+// 			query.on('end', function() {
+// 				console.log('Successfully created schema.');
+// 				done();
+// 			});
 
-			query.on('error', function(error) {
-				console.log('Error creating schema.', error);
-				process.exit(1);
-			});
-		}
-	});
-}
+// 			query.on('error', function(error) {
+// 				console.log('Error creating schema.', error);
+// 				process.exit(1);
+// 			});
+// 		}
+// 	});
+// }
 
 function initializeTask() {
 	pg.connect(connectionString, function(err, client, done) {
@@ -50,8 +50,7 @@ function initializeTask() {
 			console.log(err);
 			process.exit(1);
 		} else {
-			var query = client.query('CREATE TABLE IF NOT EXISTS task ("id" serial PRIMARY KEY, "tasks" text NOT NULL, FOREIGN KEY "user_id" int REFERENCES user (id),' + 
-				' FOREIGN KEY "category_id" int REFERENCES category (id), FOREIGN KEY "status_id" int REFERENCES status (id))');
+			var query = client.query('CREATE TABLE IF NOT EXISTS task ("id" serial PRIMARY KEY, "tasks" text NOT NULL, "status" boolean NOT NULL)');
 
 			query.on('end', function() {
 				console.log('Successfully created schema.');
@@ -66,30 +65,30 @@ function initializeTask() {
 	});
 }
 
-function initializeUser() {
-	pg.connect(connectionString, function(err, client, done) {
-		if(err) {
-			console.log(err);
-			process.exit(1);
-		} else {
-			var query = client.query('CREATE TABLE IF NOT EXISTS user ("id" serial PRIMARY KEY, "username" varchar(25) NOT NULL, "task_id" int REFERENCES task (id),' +
-				' "category_id" int REFERENCES category (id), "status_id" int REFERENCES status (id))');
+// function initializeUser() {
+// 	pg.connect(connectionString, function(err, client, done) {
+// 		if(err) {
+// 			console.log(err);
+// 			process.exit(1);
+// 		} else {
+// 			var query = client.query('CREATE TABLE IF NOT EXISTS user ("id" serial PRIMARY KEY, "username" varchar(25) NOT NULL, "task_id" int REFERENCES task (id),' +
+// 				' "category_id" int REFERENCES category (id), "status_id" int REFERENCES status (id))');
 
-			query.on('end', function() {
-				console.log('Successfully created schema.');
-				done();
-			});
+// 			query.on('end', function() {
+// 				console.log('Successfully created schema.');
+// 				done();
+// 			});
 
-			query.on('error', function(error) {
-				console.log('Error creating schema.', error);
-				process.exit(1);
-			});
-		}
-	});
-}
+// 			query.on('error', function(error) {
+// 				console.log('Error creating schema.', error);
+// 				process.exit(1);
+// 			});
+// 		}
+// 	});
+// }
 
 module.exports.connectionString = connectionString;
-module.exports.initializeStatus = initializeStatus;
-module.exports.initializeCategory = initializeCategory;
+// module.exports.initializeStatus = initializeStatus;
+// module.exports.initializeCategory = initializeCategory;
 module.exports.initializeTask = initializeTask;
-module.exports.initializeUser = initializeUser;
+// module.exports.initializeUser = initializeUser;
