de44c7df83b4a5673a82359452535aab701de067
TravisIngram <ingramtravis@gmail.com>
2016-04-25 11:24:03 -0500

    Update file to reflect single table

--- server/routes/indexRoute.js
+++ server/routes/indexRoute.js
@@ -2,14 +2,14 @@ var express = require('express');
 var path = require('path');
 
 var router = express.Router();
-var categoryRoute = require('./categoryRoute.js');
+// var categoryRoute = require('./categoryRoute.js');
 var taskRoute = require('./taskRoute');
 
 router.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname, '../public/views/index.html'));
 });
 
-router.use('/categoryRoute', categoryRoute);
+// router.use('/categoryRoute', categoryRoute);
 router.use('/taskRoute', taskRoute);
 
 module.exports = router;
