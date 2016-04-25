ca33b308e55b4fb3833bc7f738ec8850ca5a83eb
TravisIngram <ingramtravis@gmail.com>
2016-04-25 10:18:40 -0500

    Add taskRoute to indexRoute.js

--- server/routes/indexRoute.js
+++ server/routes/indexRoute.js
@@ -3,11 +3,13 @@ var path = require('path');
 
 var router = express.Router();
 var categoryRoute = require('./categoryRoute.js');
+var taskRoute = require('./taskRoute');
 
 router.get('/', function(request, response) {
 	response.sendFile(path.join(__dirname, '../public/views/index.html'));
 });
 
 router.use('/categoryRoute', categoryRoute);
+router.use('/taskRoute', taskRoute);
 
 module.exports = router;
